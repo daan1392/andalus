50a17cf0758cc43565d85ed922c38bc41568189d
daan1392 <houbendaan@gmail.com>
2026-02-25 11:41:30 +0100

    Add a test function add to add two numbers

--- src/andalus/utils.py
+++ src/andalus/utils.py
@@ -1,2 +1,7 @@
 def do_something_useful() -> None:
     print("Replace this with a utility function")
+
+
+def add(a: int, b: int) -> int:
+    """Add two numbers."""
+    return a + b
--- tests/test_andalus.py
+++ tests/test_andalus.py
@@ -1,8 +1,13 @@
 """Tests for `andalus` package."""
 
 import andalus
+from andalus.utils import add
 
 
 def test_import():
     """Verify the package can be imported."""
     assert andalus
+
+
+def test_add():
+    assert add(1, 2) == 3
