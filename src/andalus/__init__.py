d069a8a20527e43dcc62a651dbe27ea99d001970
daan1392 <houbendaan@gmail.com>
2026-03-03 11:53:48 +0100

    Added classes to __init__ for more better access.

--- src/andalus/__init__.py
+++ src/andalus/__init__.py
@@ -1,5 +1,20 @@
 """Top-level package for ANDALUS."""
 
-from andalus import application, assimilation, benchmark, covariance, sensitivity
+from .application import Application, ApplicationSuite
+from .assimilation import AssimilationSuite
+from .benchmark import Benchmark, BenchmarkSuite
+from .covariance import Covariance, CovarianceSuite
+from .sensitivity import Sensitivity
+from .utils import sandwich
 
-__all__ = ["benchmark", "sensitivity", "application", "covariance", "assimilation"]
+__all__ = [
+    "Application",
+    "ApplicationSuite",
+    "AssimilationSuite",
+    "Benchmark",
+    "BenchmarkSuite",
+    "Covariance",
+    "CovarianceSuite",
+    "Sensitivity",
+    "sandwich",
+]
